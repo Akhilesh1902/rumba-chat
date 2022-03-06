6fa0658afb0b3df9584b72a64f17741def382dc7
Akhilesh1902 <akhil1922002@gmail.com>
2022-03-06 17:06:25 +0530

    fixed the dependency array in the use effect hook in the right section file

--- src/js/components/room/RightSection.js
+++ src/js/components/room/RightSection.js
@@ -20,7 +20,7 @@ function RightSection({ socket, roomId, userName }) {
       });
       setmessages([...temp]);
     });
-  });
+  }, [socket]);
 
   useEffect(() => {
     const element = document.querySelector(".room-message-container");
